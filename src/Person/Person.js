bcbbe526b9fdd6457df3d55a3b26c1c63b92ad64
imannfazal <imannfazal@gmail.com>
2023-08-19 23:34:13 +0500

    css

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import './Person.css'
 const person = (props) => {
   return (
-    <>
+    <div className='person'>
       <p>Person name is {props.name} {Math.floor(Math.random() * 30)}</p>
       <div>{props.children}</div>
-      <input onChange={props.change} />
-    </>
+      <input onChange={props.change} value={props.name} />
+    </div>
   )
 }
 
