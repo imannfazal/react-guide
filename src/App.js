e4ece3b3f4d89a1ce33c3482e52fb46353c80781
imannfazal <imannfazal@gmail.com>
2023-08-19 19:21:57 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import { Component } from "react";
 import Person from "./Person/Person";
 
-function App() {
-  return (
-    <div>
-      <Person name='max' >Hobbies are racing, sketching</Person>
-    </div>
-  );
+class App extends Component{
+  state = {
+    persons: [
+      {name: 'max', age: 12},
+      {name: 'laiba', age: 42},
+      {name: 'me', age: 2},
+    ]
+  }
+
+  switchNameHandler= ()=>{
+    this.setState(
+      {
+        persons: [
+          {name: 'maximilian', age: 12},
+          {name: 'laiba', age: 42},
+          {name: 'iman', age: 2},
+        ]
+      }
+    );
+  }
+
+  render(){
+    return (
+      <div>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} >Hobbies are racing, sketching</Person>
+        <Person name={this.state.persons[2].name} ></Person>
+
+      </div>
+    );
+  }
 }
 
 export default App;
