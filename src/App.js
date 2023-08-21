20fe0136943d41d868faaed6db77f1ddf8793fc3
imannfazal <imannfazal@gmail.com>
2023-08-21 22:35:39 +0500

    outputting lists

--- src/App.js
+++ src/App.js
@@ -14,29 +14,29 @@ const App = () => {
     ]
   })
 
-  const switchNameHandler = () => {
-    setPersonsState(
-      {
-        persons: [
-          { name: 'maximilian', age: 12 },
-          { name: 'laiba', age: 42 },
-          { name: 'iman', age: 2 },
-        ]
-      }
-    );
-  }
+  // const switchNameHandler = () => {
+  //   setPersonsState(
+  //     {
+  //       persons: [
+  //         { name: 'maximilian', age: 12 },
+  //         { name: 'laiba', age: 42 },
+  //         { name: 'iman', age: 2 },
+  //       ]
+  //     }
+  //   );
+  // }
 
-  const nameChangedHandler = (event) => {
-    setPersonsState(
-      {
-        persons: [
-          { name: event.target.value, age: 12 },
-          { name: 'laiba', age: 42 },
-          { name: event.target.value, age: 2 },
-        ]
-      }
-    );
-  }
+  // const nameChangedHandler = (event) => {
+  //   setPersonsState(
+  //     {
+  //       persons: [
+  //         { name: event.target.value, age: 12 },
+  //         { name: 'laiba', age: 42 },
+  //         { name: event.target.value, age: 2 },
+  //       ]
+  //     }
+  //   );
+  // }
 
   const usernameHandler = (event) => {
     setUsername(event.target.value);
@@ -46,17 +46,23 @@ const App = () => {
     setShowPersons(!showPersons);
   }
 
+  let persons = null;
+
+  if (showPersons === true) {
+    persons = (
+      <div>
+        {personsState.persons.map(person =>{
+          return <Person name={person.name} age={person.age} />
+        })} 
+      </div>
+    )
+  }
+
 
   return (
     <div>
       <button onClick={togglePersonsHandler}>Show persons</button>
-      {
-        (showPersons===true) ?
-          <div>
-            <Person change={nameChangedHandler} name={personsState.persons[0].name} >Hobbies are racing, sketching</Person>
-            <Person name={personsState.persons[2].name} ></Person>
-          </div> : null
-      }
+      {persons}
       <UserInput current={userName} changed={usernameHandler} />
       <UserOuput username={userName} />
       <UserOuput username={userName} />
