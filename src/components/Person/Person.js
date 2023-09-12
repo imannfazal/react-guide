74da3a02343510ac9abde1be294ed8307f5c0300
imannfazal <imannfazal@gmail.com>
2023-09-12 20:52:05 +0500

    Update Person.js

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react'
 import './Person.css'
 import Auxilliary from '../hoc/Auxilliary';
 import withClass from '../hoc/WithClass';
-class Person extends Component{
-  render(){
-  return (
-    <Auxilliary>
-      <button onClick={this.props.click}>Delete person</button>
-      <p>Person name is {this.props.name}</p>
-      <div>{this.props.children}</div>
-      <input type='text' className='input' onChange={this.props.change} value={this.props.name} />
-    </Auxilliary>
-  )
-}}
+class Person extends Component {
+  render() {
+    return (
+      <Auxilliary>
+        <button onClick={this.props.click}>Delete person</button>
+        <p>Person name is {this.props.name}</p>
+        <div>{this.props.children}</div>
+        <input type='text' className='input' onChange={this.props.change} value={this.props.name} />
+      </Auxilliary>
+    )
+  }
+}
 
-export default withClass(Person, "person") ;
+export default withClass(Person, "person");
